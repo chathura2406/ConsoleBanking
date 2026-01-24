d64e6208cc9d4cef31c4c35d600c6e57edfc65e6
chathura2406 <it23689480@my.sliit.lk>
2026-01-25 00:14:37 +0530

    Do some Changes

--- Bank_Backend/src/main/java/com/banking/controller/AccountController.java
+++ Bank_Backend/src/main/java/com/banking/controller/AccountController.java
@@ -8,28 +8,25 @@ import java.util.Map;
 
 
 
-@RestController // 1. Meken kiyanne meka API ekak kiyala (JSON walin wada karanne).
-@RequestMapping("/api/accounts") // 2. Me class ekata enna oni Address eka. (localhost:8080/api/accounts)
+@RestController 
+@RequestMapping("/api/accounts")
 public class AccountController {
 
-    @Autowired // 3. Dependency Injection: Spring Boot ta kiyanawa "Mata Repository eke object ekak hadala denna" kiyala.
+    @Autowired
     private AccountRepository accountRepository;
 
     // --- Create Account API ---
-    @PostMapping // Meka POST request ekak (Data athulata dana ekak).
+    @PostMapping
     public Account createAccount(@RequestBody Account account) {
-        // @RequestBody: Postman eken ena JSON data tika Java Object ekak karanawa.
 
-        // Ara magic 'save' method eka call karanna witharai thiyenne!
         return accountRepository.save(account);
     }
 
-    // --- Get Account API (Data එළියට ගැනීම) ---
-    @GetMapping("/{id}") // URL එකේ අගට එන ID එක ගන්නවා (Ex: /api/accounts/1)
+    // --- Get Account API  ---
+    @GetMapping("/{id}")
     public Account getAccountById(@PathVariable Long id) {
 
-        // Repository එකෙන් අහනවා "මේ ID එක තියෙන කෙනෙක් ඉන්නවද?" කියලා.
-        // .orElseThrow(...) කියන්නේ: හොයාගන්න බැරි වුනොත් Error එකක් විසි කරන්න කියන එක.
+
         return accountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Account not found with id: " + id));
     }
@@ -38,17 +35,17 @@ public class AccountController {
     @PutMapping("/{id}/deposit") // URL eka: /api/accounts/1/deposit
     public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
 
-        // 1. Postman eken ewana 'amount' eka eliyata gannawa
+
         Double amount = request.get("amount");
 
-        // 2. Account eka Database eken hoyagannawa
+
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
 
-        // 3. Balance eka wadi karanawa (Parana Balance + Deposit Gana)
+
         account.setBalance(account.getBalance() + amount);
 
-        // 4. Aye Database ekata Save karanawa (Meken Auto UPDATE wenawa)
+
         return accountRepository.save(account);
     }
     @PutMapping("/{id}/withdraw") // URL: /api/accounts/1/withdraw
@@ -59,22 +56,22 @@ public class AccountController {
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
 
-        // MEKA THAMAI WADAGATHMA KALLA (Validation)
+
         if (account.getBalance() < amount) {
-            throw new RuntimeException("Insufficient Balance"); // Salli madi nam Error ekak denawa
+            throw new RuntimeException("Insufficient Balance");
         }
 
         account.setBalance(account.getBalance() - amount);
         return accountRepository.save(account);
     }
     // --- Delete Account API ---
-    @DeleteMapping("/{id}") // URL: /api/accounts/1
+    @DeleteMapping("/{id}")
     public String deleteAccount(@PathVariable Long id) {
 
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
 
-        accountRepository.delete(account); // Delete karana magic method eka
+        accountRepository.delete(account);
 
         return "Account Deleted Successfully!";
     }
